31699fa3c31c74127cc09d6a92441670163af1fe
Tenkazito <alejogs2931@hotmail.com>
2025-10-10 14:03:11 -0500

    Feat: subclase linea

---
+++ wFiguras/Linea.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wFiguras
+{
+    public class Linea : Figura
+    {
+        public Point Fin { get; set; }
+
+        public Linea(Point inicio, Point fin, Color color)
+            : base(inicio, color)
+        {
+            Fin = fin;
+        }
+
+        public override void Dibujar(Graphics g)
+        {
+            using (Pen lapiz = new Pen(Color, 2))
+                g.DrawLine(lapiz, Posicion, Fin);
+        }
+    }
+}
