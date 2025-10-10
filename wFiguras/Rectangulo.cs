6da214c350199c571f82d753d5faef0db300d9c2
Tenkazito <alejogs2931@hotmail.com>
2025-10-10 13:48:54 -0500

    Feat: subclase Figura

---
+++ wFiguras/Rectangulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wFiguras
+{
+    public class Rectangulo : Figura
+    {
+        public int Ancho { get; set; }
+        public int Largo { get; set; }
+
+        public Rectangulo(Point posicion, Color color, int ancho, int largo)
+            : base(posicion, color)
+        {
+            Ancho = ancho;
+            Largo = largo;
+        }
+
+        public override void Dibujar(Graphics g)
+        {
+            using (Pen lapiz = new Pen(Color))
+            {
+                g.DrawRectangle(lapiz, Posicion.X, Posicion.Y, Ancho, Largo);
+            }
+        }
+    }
+}
