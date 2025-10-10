0055789b8dd6b3969b51c4b9c7bf4b936b141e53
Tenkazito <alejogs2931@hotmail.com>
2025-10-10 13:50:29 -0500

    Feat: subclase circulo

---
+++ wFiguras/Circulo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wFiguras
+{
+    public class Circulo : Figura
+    {
+        public int Radio { get; set; }
+
+        public Circulo(Point posicion, Color color, int radio)
+            : base(posicion, color)
+        {
+            Radio = radio;
+        }
+
+        public override void Dibujar(Graphics g)
+        {
+            using (Pen lapiz = new Pen(Color))
+            {
+                g.DrawEllipse(lapiz, Posicion.X, Posicion.Y, Radio * 2, Radio * 2);
+            }
+        }
+    }
+}
