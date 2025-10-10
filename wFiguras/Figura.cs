db2fa6e464e11569f3f91ed763a1f8c1c05fdc6e
Tenkazito <alejogs2931@hotmail.com>
2025-10-10 13:44:26 -0500

    Feat: clase base figura

---
+++ wFiguras/Figura.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wFiguras
+{
+    public abstract class Figura
+    {
+        public Color Color { get; set; }
+        public Point Posicion { get; set; }
+
+        public Figura(Point posicion, Color color)
+        {
+            Posicion = posicion;
+            Color = color;
+        }
+
+        public abstract void Dibujar(Graphics g);
+    }
+}
